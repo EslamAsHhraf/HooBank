9773a7c74c4d125cbaf6b8ec203208bd477c8140
Eslam Ashraf <71986226+EslamAsHhraf@users.noreply.github.com>
2022-09-19 15:42:04 +0200

    Add Project ✅✅

---
+++ src/components/index.js
@@ -0,0 +1,23 @@
+import Navbar from "./Navbar/Navbar";
+import Billing from "./Billing";
+import CardDeal from "./CardDeal";
+import Business from "./Business/Business";
+import Clients from "./Clients";
+import CTA from "./CTA";
+import Stats from "./Hero/Stats";
+import Footer from "./Footer/Footer";
+import Testimonials from "./Testimonials/Testimonials";
+import Hero from "./Hero/Hero";
+
+export {
+  Navbar,
+  Billing,
+  CardDeal,
+  Business,
+  Clients,
+  CTA,
+  Stats,
+  Footer,
+  Testimonials,
+  Hero,
+};
